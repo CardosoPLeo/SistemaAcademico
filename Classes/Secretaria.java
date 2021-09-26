e1b3ed0c1a46cff3b8eb26b13dd37c5817a36abe
CardosoPLeo <leonardo_p_c@hotmail.com>
2021-09-26 19:13:37 -0300

    acertada a identação da classe

--- Classes/Secretaria.java
+++ Classes/Secretaria.java
@@ -3,10 +3,56 @@ package SistemaAcademico;
 import java.util.Collection;
 
 public class Secretaria{
-	
-	public Int Codigo;
-	public String Nome;
-	public List<Cursos> curso;
-	public List<Coordenador> coordenador;
-	
+
+	private Int Codigo;
+	private String Nome;
+	private List<Disciplinas> disciplina;
+	private List<Cursos> curso;
+	private List<Coordenador> coordenador;
+
+	public Secretaria() {
+
+		super();
+	}
+
+	public List<Coordenador> getCoordenador() {
+		return coordenador;
+	}
+
+	public void setCoordenador(List<Coordenador> coordenador) {
+		this.coordenador = coordenador;
+	}
+
+	public List<Cursos> getCurso() {
+		return curso;
+	}
+
+	public void setCurso(List<Cursos> curso) {
+		this.curso = curso;
+	}
+
+	public List<Disciplinas> getDisciplina() {
+		return disciplina;
+	}
+
+	public void setDisciplina(List<Disciplinas> disciplina) {
+		this.disciplina = disciplina;
+	}
+
+	public String getNome() {
+		return Nome;
+	}
+
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+
+	public Int getCodigo() {
+		return Codigo;
+	}
+
+	public void setCodigo(Int codigo) {
+		Codigo = codigo;
+	}
+
 }
