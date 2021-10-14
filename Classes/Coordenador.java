a3a52f418bf51d786b04e32fa84d3c29ee994321
CardosoPLeo <leonardo_p_c@hotmail.com>
2021-10-13 23:26:17 -0300

    Implementado a classe de coordenador

--- Classes/Coordenador.java
+++ Classes/Coordenador.java
@@ -1,1 +1,42 @@
 package SistemaAcademico;
+
+public class Coordenador {
+
+
+
+    private String nome;
+    private String Email;
+
+    public Coordenador() {
+        super();
+    }
+
+    public Int getCodigo() {
+        return codigo;
+    }
+
+    private Int codigo;
+
+    public String getNome() {
+        return nome;
+    }
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public void setCodigo(Int codigo) {
+        this.codigo = codigo;
+    }
+
+    public void alocaProfessorATurma*(){
+
+    }
+}
