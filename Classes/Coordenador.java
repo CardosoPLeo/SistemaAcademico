5f33a2ff49ac66171c8e22d5d2098985dcdd1d48
CardosoPLeo <leonardo_p_c@hotmail.com>
2021-10-13 23:38:21 -0300

    Reformulada a classe de Coordenador

--- Classes/Coordenador.java
+++ Classes/Coordenador.java
@@ -1,47 +1,73 @@
 package SistemaAcademico;
 
+import Professor;
+import Turma;
+
+import java.util.List;
+
 public class Coordenador {
 
     private String nome;
     private String email;
     private Int codigo;
-
+    private List<Professor> professor;
+    private List<Turma> turma;
 
     public Coordenador() {
         super();
     }
 
-    public Coordenador(String nome, String email, Int codigo) {
+    public Coordenador(String nome, String email, Int codigo, List<Professor> professor, List<Turma> turma) {
         super();
         this.nome = nome;
         this.email = email;
         this.codigo = codigo;
-    }
-
-    public Int getCodigo() {
-        return codigo;
+        this.professor = professor;
+        this.turma = turma;
     }
 
     public String getNome() {
         return nome;
-    }
-    public String getEmail() {
-        return email;
     }
 
     public void setNome(String nome) {
         this.nome = nome;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
     public void setEmail(String email) {
-        email = email;
+        this.email = email;
+    }
+
+    public Int getCodigo() {
+        return codigo;
     }
 
     public void setCodigo(Int codigo) {
         this.codigo = codigo;
     }
 
-    public void alocaProfessorATurma*(){
+    public List<Professor> getProfessor() {
+        return professor;
+    }
+
+    public void setProfessor(List<Professor> professor) {
+        this.professor = professor;
+    }
+
+    public List<Turma> getTurma() {
+        return turma;
+    }
+
+    public void setTurma(List<Turma> turma) {
+        this.turma = turma;
+    }
+
+    public void alocaProfessorATurma(){
+
 
     }
 }
